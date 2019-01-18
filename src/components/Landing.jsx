a3cac62e34d06bf96bce02c8c38d2404c6a41725
Luthfi Aji <luthfi.ajiw@gmail.com>
2019-01-18 13:42:07 +0700

    Jumbotron & Navbar

--- src/components/Landing.jsx
+++ src/components/Landing.jsx
@@ -4,9 +4,18 @@ import Navbar from './Navbar';
 class Landing extends Component {
     render() {
         return (
-            <div className="landing">
+            <section className="landing">
               <Navbar/>
-            </div>
+
+              <section className="jumbotron">
+                <section className="jumbo-text">
+                  <h1 className="display-4">Hello, world!</h1>
+                  <p className="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                  <hr className="my-4"/>
+                </section>
+                <button className="ml-4 my-5 btn btn-primary btn-lg" href="#" role="button">Learn more</button>
+              </section>
+            </section>
         );
     }
 }
